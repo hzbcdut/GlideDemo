5a7c676b4e3dcb7c897b6744d842fb510bd9f9cb
huzhengbiao <hans.hu@livestar.com>
2017-01-17 10:49:38 +0800

    通知自定义GlideModule指定下载目录， 必须指定value的值是“GlideModule”才能成功加载

--- app/src/main/java/hzb/cdut/com/glidedemo/module/CustomCachingGlideModule.java
+++ app/src/main/java/hzb/cdut/com/glidedemo/module/CustomCachingGlideModule.java
@@ -7,17 +7,21 @@ import com.bumptech.glide.GlideBuilder;
 import com.bumptech.glide.load.engine.cache.DiskLruCacheFactory;
 import com.bumptech.glide.module.GlideModule;
 
+import hzb.cdut.com.glidedemo.constant.Constant;
 import hzb.cdut.com.glidedemo.utils.FileUtil;
+import hzb.cdut.com.glidedemo.utils.LogUtil;
 
 /**
  * Created by hans on 2017/1/16 0016.
  */
 
 public class CustomCachingGlideModule implements GlideModule{
+    private static final String TAG = "CustomCachingGlideModule";
     @Override
     public void applyOptions(Context context, GlideBuilder builder) {
         int cacheSize100MegaBytes = 104857600;
         String downloadDirectoryPath = FileUtil.getFile();
+        LogUtil.d(Constant.Debug,TAG + "--> downloadDirectoryPath =" + downloadDirectoryPath);
         builder.setDiskCache(new DiskLruCacheFactory(downloadDirectoryPath, cacheSize100MegaBytes));
     }
 
